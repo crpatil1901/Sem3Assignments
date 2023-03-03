71f76a789789c730fc4f5d48e58d2cc59f9d2022
crpatil1901 <81147845+crpatil1901@users.noreply.github.com>
2023-03-03 15:30:25 +0530

    Update assignment3.cpp

--- DSAL/assignment3.cpp
+++ DSAL/assignment3.cpp
@@ -3,20 +3,23 @@
 using namespace std;
 
 template <typename T>
-class ThreadedBinarySearchTree {
+class ThreadedBinarySearchTree
+{
 public:
     T data;
     ThreadedBinarySearchTree *l, *r;
     bool rThrd, isEmpty;
 
-    ThreadedBinarySearchTree() {
+    ThreadedBinarySearchTree()
+    {
         this->isEmpty = true;
         this->rThrd = false;
         this->r = NULL;
         this->l = NULL;
     }
 
-    ThreadedBinarySearchTree(T data) {
+    ThreadedBinarySearchTree(T data)
+    {
         this->isEmpty = false;
         this->data = data;
         this->rThrd = false;
@@ -24,30 +27,46 @@ public:
         this->l = NULL;
     }
 
-    void insert(T data) {
-        if (this->isEmpty) {
+    void insert(T data)
+    {
+        if (this->isEmpty)
+        {
             this->isEmpty = false;
             this->data = data;
-        } else {
-            if (data < this->data) {
-                if (this->l) {
+        }
+        else
+        {
+            if (data < this->data)
+            {
+                if (this->l)
+                {
                     this->l->insert(data);
-                } else {
+                }
+                else
+                {
                     this->l = new ThreadedBinarySearchTree(data);
                     this->l->r = this;
                     this->l->rThrd = true;
                 }
-            } else {
-                if (this->r && !(this->rThrd)) {
+            }
+            else
+            {
+                if (this->r && !(this->rThrd))
+                {
                     this->r->insert(data);
-                } else {
-                    if (this->rThrd) {
+                }
+                else
+                {
+                    if (this->rThrd)
+                    {
                         this->rThrd = false;
                         ThreadedBinarySearchTree *temp = new ThreadedBinarySearchTree(data);
                         temp->rThrd = true;
                         temp->r = this->r;
                         this->r = temp;
-                    } else {
+                    }
+                    else
+                    {
                         this->r = new ThreadedBinarySearchTree(data);
                     }
                 }
@@ -55,51 +74,191 @@ public:
         }
     }
 
-    void show() {
+    void show()
+    {
         ThreadedBinarySearchTree *current = this;
-        while (current->l) { current = current->l; }
+        while (current->l)
+        {
+            current = current->l;
+        }
 
-        while (current) {
+        while (current)
+        {
 
-            cout<<"Node: "<<current->data<<" Left: ";
-            if (current->l) {
-                cout<<current->l->data;
-            } else {
-                cout<<"NULL";
+            cout << "Node: " << current->data << " Left: ";
+            if (current->l)
+            {
+                cout << current->l->data;
             }
-            cout<<" Right: ";
-            if (current->r) {
-                cout<<current->r->data;
-            } else {
-                cout<<"NULL";
+            else
+            {
+                cout << "NULL";
             }
-            if (current->rThrd) {
-                cout<<" Threaded";
-            } else {
-                cout<<" Not Threaded";
+            cout << " Right: ";
+            if (current->r)
+            {
+                cout << current->r->data;
             }
-            cout<<"\n";
-            
-            if (!(current->l) && !(current->r)) {
+            else
+            {
+                cout << "NULL";
+            }
+            if (current->rThrd)
+            {
+                cout << " Threaded";
+            }
+            else
+            {
+                cout << " Not Threaded";
+            }
+            cout << "\n";
+
+            if (!(current->l) && !(current->r))
+            {
                 return;
             }
 
-            
-            if (current->rThrd) {
+            if (current->rThrd)
+            {
                 current = current->r;
-            } else if (current->r) {
+            }
+            else if (current->r)
+            {
                 current = current->r;
-                while (current->l) {
+                while (current->l)
+                {
                     current = current->l;
                 }
             }
-
         }
-        
+    }
+
+    void preOrder()
+    {
+        if (this->isEmpty)
+        {
+            cout << "Empty\n";
+        }
+        ThreadedBinarySearchTree<T> *current = this;
+        while (1)
+        {
+            cout << current->data << " ";
+            if (current->l)
+            {
+                current = current->l;
+            }
+            else if (current->r)
+            {
+                while (current->rThrd)
+                {
+                    current = current->r;
+                }
+                if (current->r)
+                {
+                    current = current->r;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                cout << "\n";
+                return;
+            }
+        }
+    }
+
+    void remove(T key)
+    {
+        if (this->isEmpty)
+        {
+            return;
+        }
+
+        ThreadedBinarySearchTree<T> *current = this;
+        ThreadedBinarySearchTree<T> *prev = NULL;
+        while (1)
+        {
+            if (current->data == key)
+            {
+                if (!current->l && (current->rThrd || !current->r))
+                {
+                    if (current == prev->l)
+                    {
+                        delete prev->l;
+                        prev->l = NULL;
+                    }
+                    else
+                    {
+                        ThreadedBinarySearchTree<T> *temp = current->r;
+                        delete prev->r;
+                        prev->r = temp;
+                        if (temp)
+                        {
+                            prev->rThrd = true;
+                        }
+                    }
+                    return;
+                }
+                else if (current->l && !current->rThrd && current->r)
+                {
+                    ThreadedBinarySearchTree<T> *successor = current->r;
+                    while (successor->l)
+                    {
+                        successor = successor->l;
+                    }
+                    current->data = successor->data;
+                    key = successor->data;
+                    current = current->r;
+                }
+                else
+                {
+                    if (current->l)
+                    {
+                        current->data = current->l->data;
+                        key = current->l->data;
+                        current = current->l;
+                    }
+                    else
+                    {
+                        current->data = current->r->data;
+                        key = current->r->data;
+                        current = current->r;
+                    }
+                }
+            }
+            else if (current->data < key)
+            {
+                if (current->r)
+                {
+                    prev = current;
+                    current = current->r;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                if (current->l)
+                {
+                    prev = current;
+                    current = current->l;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
     }
 };
 
-int main() {
+int main()
+{
     ThreadedBinarySearchTree<int> tbst;
 
     tbst.insert(5);
@@ -108,10 +267,13 @@ int main() {
     tbst.insert(2);
     tbst.insert(1);
     tbst.insert(4);
-    tbst.insert(8);
     tbst.insert(9);
+    tbst.insert(8);
     tbst.insert(6);
     tbst.insert(10);
     tbst.show();
 
+    tbst.remove(2);
+    tbst.show();
+    tbst.preOrder();
 }
